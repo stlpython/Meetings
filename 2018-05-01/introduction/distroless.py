4304046c605e47e140fc2dcf3b0bfde39360deb6
Andrew Latham <lathama@gmail.com>
2018-05-01 16:27:30 -0500

    save early and often

--- 2018-05-01/introduction/distroless.py
+++ 2018-05-01/introduction/distroless.py
@@ -12,18 +12,17 @@ import cherrypy
 host = "::"
 port = 8080
 
-pwd = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
 conf = {
-    '/': { 'tools.staticdir.root': pwd },
+    '/': { 'tools.staticdir.root': "/srv/app/static" },
     '/static':
         {
             'tools.staticdir.on': True,
-            'tools.staticdir.dir': "static"
+            'tools.staticdir.dir': "/srv/app/static"
         },
     '/favicon.ico':
         {
             'tools.staticfile.on': True,
-            'tools.staticfile.filename': pwd + "static/favicon.ico"
+            'tools.staticfile.filename': "/srv/app/static/favicon.ico"
         },
     }
 
@@ -31,7 +30,7 @@ api_conf = {
     '/favicon.ico':
         {
             'tools.staticfile.on': True,
-            'tools.staticfile.filename': pwd + "static/favicon.ico"
+            'tools.staticfile.filename': "/srv/app/static/favicon.ico"
         },
 }
 
@@ -53,6 +52,32 @@ class DistrolessDemo(object):
     def u(self):
         return "users"
 
+    @cherrypy.expose
+    def file(self):
+        return """
+        <html><h3>Upload a file</h3>
+            <form action="/upload" method="post" enctype="multipart/form-data">
+            Filename: <input type="file" name="myFile" />
+            <input type="submit" /></form></html>
+        """
+
+    @cherrypy.expose
+    def upload(self, myFile):
+        out = """<html></pre>
+            myFile length: %s<br />
+            myFile filename: %s<br />
+            myFile mime-type: %s
+        </pre>
+        </html>"""
+        size = 0
+        while True:
+            data = myFile.file.read(8192)
+            if not data:
+                break
+            size += len(data)
+        return out % (size, myFile.filename, myFile.content_type)
+
+
 class DistrolessDemo_API(object):
     @cherrypy.expose
     def index(self):
@@ -60,11 +85,16 @@ class DistrolessDemo_API(object):
 
     @cherrypy.expose
     def status(self):
-        return "running"
+        return "<html>Things are looking: Good </html>"
+
+    @cherrypy.expose
+    def r(self):
+        import random
+        return str(random.randint(9999, 99999))
 
     @cherrypy.expose
     def download(self):
-        return "download stuffs"
+        return "</html><h1>Download Things</h1></html>"
 
 if __name__ == '__main__':
     cherrypy.tree.mount(DistrolessDemo(), '/', conf)
